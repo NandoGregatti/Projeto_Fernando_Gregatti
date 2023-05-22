20d34c8d2b0e9232fc9a6c7781e25e49e982c3d3
NandoGregatti <fernando.noronha1@estudante.ufla.br>
2023-05-22 15:38:46 -0300

    Criada a classe D

---
+++ classD.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class D {
+    private:
+        int D1;
+        int D2;
+    public:
+        D() {
+            D1 = 0;
+            D2 = 0;
+        }
+
+        void MD1();
+        void MD2();
+};
+
+void D::MD1() {
+    cout<<"MD1"<<endl;
+};
+
+void D::MD2() {
+    cout<<"MD2"<<endl;
+};

d71ea4f8e1857b79771d99255f869564d24d3a27
NandoGregatti <fernando.noronha1@estudante.ufla.br>
2023-05-22 16:28:04 -0300

    Remove o MD3()

--- classD.cpp
+++ classD.cpp
@@ -14,7 +14,7 @@ class D {
 
         void MD1();
         void MD2();
-        void MD3();
+
 };
 
 void D::MD1() { 
@@ -23,8 +23,4 @@ void D::MD1() {
 
 void D::MD2() {
     cout<<"MD2"<<endl;
-};
-
-void D::MD3() {
-    cout<<"MD3"<<endl;
 };
